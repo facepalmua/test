8377ed792d2ff252b69abd3495bc2e752fc4bb5e
Bogdan Zakharchyshyn <facepalmua@gmail.com>
2021-01-21 15:15:26 +0200

    new commit

--- js/src.js
+++ js/src.js
@@ -4,12 +4,26 @@ jQuery(document).ready(function($) {
   if($('.location-city').length){
     navigator.geolocation.getCurrentPosition(
       function(position){
-        var geocoder = new google.maps.Geocoder;
-        var latlng = {lat: position.coords.latitude, lng: position.coords.longitude};
-          geocoder.geocode({'location': latlng}, function(results, status) {
-          let city = results[0].address_components[3].short_name;
-					console.log('city ', city);
-					console.log('city ', results);
+        let geocoder = new google.maps.Geocoder;
+        let latlng = {lat: position.coords.latitude, lng: position.coords.longitude};
+
+        geocoder.geocode({'location': latlng}, function(results, status) {
+
+          let checkTypeCity = 'locality';
+
+          for(let i = 0; i < results.length; i++){
+            for(let j = 0; j < results[i].address_components.length; j++){
+
+              let searchResult = results[i].address_components[j].types[0]
+
+              if(searchResult == checkTypeCity){
+                let setNameCity = results[i].address_components[j].short_name
+                $('.location-city').find('b').html('м. ' + setNameCity)
+              }
+
+            }
+          }
+
         });
       }
     );
